65bfdac7f25d8800d5a9098224b144c2c60c9e50
manish.kumar <manish.kumar@kochartech.com>
2023-03-03 16:31:01 +0530

    create form

--- src/Component/AddEmployee.js
+++ src/Component/AddEmployee.js
@@ -1,26 +1,71 @@
 import React, { useState, useEffect } from "react";
+import {useDispatch} from "react-redux"
 
 const AddEmployee = () => {
   const [employee, setEmployee] = useState({
-    name: "manish",
+    name: "",
     email: "",
     phone: "",
     wife: "",
   });
 
-  const {name, email, phone, wife} = employee
-
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setEmployee((prevState) => ({ ...prevState, [name]: value }));
+    setEmployee({ ...employee, [name]: value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("employee inside submit", employee)
+
+  }
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={employee.name}
+          onChange={handleInputChange}
+        />
+        <br />
 
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={employee.email}
+          onChange={handleInputChange}
+        />
+        <br />
+
+        <label htmlFor="phone">Phone:</label>
+        <input
+          type="tel"
+          id="phone"
+          name="phone"
+          value={employee.phone}
+          onChange={handleInputChange}
+        />
+        <br />
+
+        <label htmlFor="wife">Wife:</label>
+        <input
+          type="text"
+          id="wife"
+          name="wife"
+          value={employee.wife}
+          onChange={handleInputChange}
+        />
+        <br />
+        <input type="submit" />
+      </form>
     </div>
-  )
+  );
 };
 
 export default AddEmployee;
